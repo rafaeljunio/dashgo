b2e3ef2f776f9f492e39c05b4ae56e3530d6a526
Rafael Souza <rafael.souza@iexfy.com>
2022-07-09 08:25:10 -0300

    class 1 finished

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,8 @@
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
+
+export function Sidebar() {
+  return (
+    <h1>Sidebar</h1>
+  );
+}
