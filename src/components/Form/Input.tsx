b37ac0689b64e8dfb35fc313bf588a4fe6863512
Rafael Souza <rafael.souza@iexfy.com>
2022-07-11 21:13:18 -0300

    react hook form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,28 +1,34 @@
 import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react'
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
-  return (
-    <FormControl>
-      {/* Caso o label existir exibe o componente FormLabel */}
-      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-      <ChakraInput
-        name={name}
-        id={name}
-        type="email"
-        focusBorderColor="pink.500"
-        bgColor="gray.900"
-        variant="filled"
-        _hover={{
-          bgColor: 'gray.900'
-        }}
-        size="lg"
-        {...rest}
-      />
-    </FormControl>
-  )
-}
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
+  = ({ name, label, ...rest }, ref) => {
+    return (
+      <FormControl>
+        {/* Caso o label existir exibe o componente FormLabel */}
+        {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+        <ChakraInput
+          name={name}
+          id={name}
+          type="email"
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: 'gray.900'
+          }}
+          ref={ref}
+          size="lg"
+          {...rest}
+        />
+      </FormControl>
+    )
+  }
+
+export const Input = forwardRef(InputBase);
+
