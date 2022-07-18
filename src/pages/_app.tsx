60aa0c8a94b1d49efea005795c2fa0ff65915a3a
Rafael Souza <rafael.souza@animasales.com.br>
2022-07-17 23:18:58 -0300

    feat: factories e seeds

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,12 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
 
+// variável de ambiente setada automaticamente pelo next
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
 
